4a73a1065c244f309f4817062f450c013d0120ae
Ziesche Til Newman <tilnewman@protonmail.com>
2024-08-03 09:48:59 -0600

    update map paths

--- media/tiled/object-2.tsx
+++ media/tiled/object-2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="object-2" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../../../src/halloween/media/image/map/tile-object-2.png" width="512" height="512"/>
+ <image source="../image/map/tile-object-2.png" width="512" height="512"/>
 </tileset>
